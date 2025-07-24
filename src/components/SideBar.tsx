42f347b717e6dd38e2bef71079d70682c674609c
iiamsona <sona22hakobyan@gmail.com>
2025-07-24 23:27:37 +0400

    feat:scroll_bar_hover_full

--- src/components/SideBar.tsx
+++ src/components/SideBar.tsx
@@ -1,4 +1,5 @@
-import SideBarButton from './SideBarButton.tsx'
+import { useState } from "react";
+import SideBarButton from "./SideBarButton.tsx";
 import Search from "../assets/icons/search.png";
 import Home from "../assets/icons/home.png";
 import Video from "../assets/icons/video.png";
@@ -7,16 +8,37 @@ import Mask from "../assets/icons/mask.png";
 import History from "../assets/icons/history.png";
 
 const SideBar = () => {
+  const [hovered, setHovered] = useState(true);
   return (
-    <section className="fixed z-50 top-0 left-0 h-full w-36 hover:w-96 flex flex-col justify-center items-center hover:items-start hover:pl-2  sidebar_hover">
-      <div className="flex flex-col gap-2">
-        <SideBarButton image={Search} alt='Search' />
-        <SideBarButton image={Home} alt='Home' selected />
-        <SideBarButton image={Video} alt='Video' />
-        <SideBarButton image={Reel} alt='Reel' />
-        <SideBarButton image={Mask} alt='Mask' />
-        <SideBarButton image={History} alt='History' />
+    <section
+      className={`fixed z-50 top-0 left-0 h-full w-36 hover:w-96 flex flex-col items-center hover:items-start hover:pl-2  sidebar_hover ${hovered?'justify-between': 'justify-center'}`}
+      onMouseEnter={() => setHovered(true)}
+      onMouseLeave={() => setHovered(false)}
+    >
+      <div
+        className={`w-full flex flex-col gap-2 ${
+          hovered ? "items-start pl-2 pr-16" : "items-center"
+        }`}
+      >
+      {hovered && (  <div className="flex items-center justify-center gap-3 pt-6 pl-2">
+            <div className="w-12 h-12 bg-fuchsia-300 rounded-full"></div>
+            <p className="font-semibold">Daniel</p>
+        </div>
+      )}
+        <SideBarButton image={Search} alt="Search" hovered={hovered} />
+        <SideBarButton image={Home} alt="Home" selected hovered={hovered} />
+        <SideBarButton image={Video} alt="TV shows" hovered={hovered} />
+        <SideBarButton image={Reel} alt="Movies" hovered={hovered} />
+        <SideBarButton image={Mask} alt="Ganres" hovered={hovered} />
+        <SideBarButton image={History} alt="Watch Later" hovered={hovered} />
       </div>
+      {hovered && (
+        <div className="w-full grid items-start pl-8 pb-6 gap-1 text-md font-semibold tracking-widest text-gray-400 uppercase">
+          <p>Langage</p>
+          <p>Get help</p>
+          <p>Exit</p>
+        </div>
+      )}
     </section>
   );
 };
